f6886487c4bae1367914917c3a24e2a72f9958db
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-04-21 14:56:43 +0300

    add configurations

---
+++ src/SearchHub.Api/Configuration/SearchHubConfiguration.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SearchHub.Api.Configuration
+{
+    public class SearchHubConfiguration
+    {
+        public List<SiteConfiguration> Sites {  get; init; }
+    }
+
+    public class SiteConfiguration
+    {        
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+     
+        public string Url { get; init; }
+    }
+}
